1511acb620ab0cc65b1890870f87299365c5207c
Nurami <MrNursil@gmail.com>
2016-02-28 20:26:41 +0300

    qq

---
+++ AlgotitmKnuta/src/ru/itis/inform/Human.java
@@ -0,0 +1,17 @@
+package ru.itis.inform;
+
+/**
+ * Created by Nurami on 26.02.2016.
+ */
+public class Human {
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+}
---
+++ AlgotitmKnuta/src/ru/itis/inform/Main.java
@@ -0,0 +1,10 @@
+package ru.itis.inform;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Human mass[] = new Human[3];
+        KnutCode a = new KnutCode();
+        a.indexesOf();
+    }
+}
